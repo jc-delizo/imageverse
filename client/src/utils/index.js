d8579ed2c90fb6f1f77f02fe31e9e41d899c7cf3
JC Delizo <mrjcdelizo@gmail.com>
2023-04-10 16:13:08 +0800

    First Commit

---
+++ client/src/utils/index.js
@@ -0,0 +1,16 @@
+import FileSaver from 'file-saver';
+
+import { surpriseMePrompts } from '../constants';
+
+export function getRandomPrompt(prompt) {
+    const randomIndex = Math.floor(Math.random() * surpriseMePrompts.length);
+    const RandomPrompt = surpriseMePrompts[randomIndex]
+
+    if(RandomPrompt === prompt) return getRandomPrompt(prompt)
+
+    return RandomPrompt;
+}
+
+export async function downloadImage(_id, photo) {
+    FileSaver.saveAs(photo, `download-${_id}.jpg`);
+}
